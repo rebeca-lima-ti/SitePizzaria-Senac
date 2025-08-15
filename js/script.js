469cdaca024f5ab436f4499d8b445c878d86d38a
Rebeca Lima <becaln.ti0@gmail.com>
2025-08-14 22:27:36 -0300

    Add blog and slider sections, improve styles and interactivity
    Introduces new 'slidder' and 'blog' sections to the homepage, including corresponding CSS for layout and hover effects. Enhances card and button interactivity, updates responsive breakpoints and styles, and adds AOS and FontAwesome for animations and icons. JavaScript is updated to initialize AOS and handle hover image changes for both cards and slider items.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+// Inicialização do AOS
+AOS.init();
+
 window.addEventListener("scroll", function () {
   const navbar = document.querySelector(".navbar");
   if (window.scrollY > 50) {
@@ -13,6 +16,22 @@ cards.forEach(card => {
   const img = card.querySelector('.img-pizza');
   const srcDefault = img.getAttribute('src');
   const srcHover = srcDefault.replace('.png', '_color.png'); 
+
+  card.addEventListener('mouseenter', () => {
+    img.setAttribute('src', srcHover);
+  });
+
+  card.addEventListener('mouseleave', () => {
+    img.setAttribute('src', srcDefault);
+  });
+});
+
+const slidders = document.querySelectorAll('.slidder');
+
+slidders.forEach(card => {
+  const img = card.querySelector('.img-slidder');
+  const srcDefault = img.getAttribute('src');
+  const srcHover = srcDefault.replace('_pb.svg', '.svg'); 
 
   card.addEventListener('mouseenter', () => {
     img.setAttribute('src', srcHover);
